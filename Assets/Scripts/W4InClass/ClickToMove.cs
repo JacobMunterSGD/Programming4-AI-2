4981214bfcb9dd082c7d01c3338f4e10fed26db0
JacobMunterSGD <148461541+JacobMunterSGD@users.noreply.github.com>
2025-01-27 11:55:12 -0500

    in class stuff

---
+++ Assets/Scripts/W4InClass/ClickToMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour
+{
+    public NavMeshAgent navAgent;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(mouseRay, out RaycastHit hitInfo))
+            {
+                navAgent.SetDestination(hitInfo.point);
+            }
+
+        }
+    }
+}
